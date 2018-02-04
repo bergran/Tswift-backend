b6a5de72092b71ed93bf34187ec35cfe4a75b6da
Ángel Berhó <bergraan@gmail.com>
2018-02-04 22:41:36 +0100

    Added Docs

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.documentation import include_docs_urls
 from rest_framework_jwt.views import verify_jwt_token, refresh_jwt_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/docs/', include_docs_urls(title='Api docs', public=False)),
     path('api/v1/', include('v1.urls')),
     path('api/token/verify', verify_jwt_token),
     path('api/token/refresh', refresh_jwt_token)
