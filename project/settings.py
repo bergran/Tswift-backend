812a7600c2a42619b7efa8548416307a246a9c30
Ángel Berhó <bergraan@gmail.com>
2018-02-10 14:25:15 +0100

    Added test settings

--- project/settings.py
+++ project/settings.py
@@ -144,6 +144,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     )
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 JWT_AUTH = {
