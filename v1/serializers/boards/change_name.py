7de26d9181084fb9b41b7f0157246bd642534022
Ángel Berhó <bergraan@gmail.com>
2018-02-10 14:27:48 +0100

    Refactor ChangeName serializer

--- v1/serializers/boards/change_name.py
+++ v1/serializers/boards/change_name.py
@@ -2,6 +2,15 @@
 
 from rest_framework import serializers
 
+from v1.models.Board import Boards
 
-class ChangeNameSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=20)
+
+class ChangeNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Boards
+        fields = ('id', 'name', 'date_created', 'deleted')
+        read_only_fields = (
+            'id',
+            'date_created',
+            'deleted'
+        )
