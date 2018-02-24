f5ef0982731df5a4d945901f3660709993eda7a4
Ángel Berhó <bergraan@gmail.com>
2018-02-24 12:07:54 +0100

    Refactor permissions

--- v1/permissions/states/permissions.py
+++ v1/permissions/states/permissions.py
@@ -4,50 +4,24 @@ from django.db.models import Q
 from rest_framework.permissions import BasePermission
 
 
-class BoardPermission(BasePermission):
+class StatesPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if view.action == 'retrieve':
-            user = request.user
-            is_owner = user.boards_set.filter(pk=obj.pk).exists()
-            has_group_perm = obj.groupboardpermissions_set.filter(
-                group__in=user.groups.all(), permission__name='read'
-            ).exists()
-            has_user_perm = obj.userboardpermissions_set.filter(
-                user=user, permission__name='read'
-            ).exists()
+        if view.action in ['get_states', 'retrieve']:
+            permissions_name = ['read']
         elif view.action == 'destroy':
-            user = request.user
-            is_owner = user.boards_set.filter(pk=obj.pk).exists()
-            has_group_perm = obj.groupboardpermissions_set.filter(
-                Q(group__in=user.groups.all()) &
-                Q(permission__name__in=['delete', 'read'])
-            ).count() == 2
-            has_user_perm = obj.userboardpermissions_set.filter(
-                Q(user=user) &
-                Q(permission__name__in=['delete', 'read'])
-            ).count() == 2
+            permissions_name = ['delete', 'read']
         elif view.action == 'change_name':
-            user = request.user
-            is_owner = user.boards_set.filter(pk=obj.pk).exists()
-            has_group_perm = obj.groupboardpermissions_set.filter(
-                Q(group__in=user.groups.all()) &
-                Q(permission__name__in=['delete', 'read', 'write'])
-            ).count() == 3
-            has_user_perm = obj.userboardpermissions_set.filter(
-                Q(user=user) &
-                Q(permission__name__in=['delete', 'read', 'write'])
-            ).count() == 3
-        elif view.action == 'get_states':
-            user = request.user
-            is_owner = user.boards_set.filter(pk=obj.pk).exists()
-            has_group_perm = obj.groupboardpermissions_set.filter(
-                Q(group__in=user.groups.all()) &
-                Q(permission__name__in=['read'])
-            ).exists()
-            has_user_perm = obj.userboardpermissions_set.filter(
-                Q(user=user) &
-                Q(permission__name__in=['read'])
-            ).exists()
+            permissions_name = ['read', 'write']
         else:
             return True
+        user = request.user
+        is_owner = obj.board.owner == user
+        has_group_perm = obj.board.groupboardpermissions_set.filter(
+            group__in=user.groups.all(),
+            permission__name__in=permissions_name
+        ).count() == len(permissions_name)
+        has_user_perm = obj.board.userboardpermissions_set.filter(
+            user=user,
+            permission__name__in=permissions_name
+        ).count() == len(permissions_name)
         return is_owner or has_group_perm or has_user_perm
