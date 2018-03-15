2abbb05d43a2b7bf3ea60a4661afb8c114821c74
Ángel Berhó <bergraan@gmail.com>
2018-03-15 21:49:00 +0100

    Added managers to Board

--- v1/models/Board.py
+++ v1/models/Board.py
@@ -3,6 +3,13 @@
 from django.contrib.auth import models as django_models
 from django.db import models
 
+class BoardManager(models.Manager):
+    def get_boards_access(self, user):
+        return self.filter(
+            Q(owner=user) |
+            Q(userboardpermissions__user=user, userboardpermissions__permission__name='read') |
+            Q(groupboardpermissions__group__in=user.groups.all(), groupboardpermissions__permission__name='read')
+        ).distinct()
 
 class Boards(models.Model):
     name = models.CharField(max_length=20)
@@ -10,6 +17,10 @@ class Boards(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
+
+    # Managers
+    permissions = BoardManager()
+    objects = models.Manager()
 
     class Meta:
         db_table = 'Boards'
