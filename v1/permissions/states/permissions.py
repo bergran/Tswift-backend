bb2165dd183cd7e80ce8792db94bfe8f58b526b0
Ángel Berhó <bergraan@gmail.com>
2018-03-25 16:26:39 +0200

    Refactor states permissions

--- v1/permissions/states/permissions.py
+++ v1/permissions/states/permissions.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from django.db.models import Q
-
 from rest_framework.permissions import BasePermission
 
+from v1.models.Board import Boards
 
 class StatesPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -22,16 +21,8 @@ class StatesPermission(BasePermission):
             permissions_name = ['read', 'write', 'delete']
 
         user = request.user
-        if obj.board.owner == user:
-            return True
-        elif obj.board.groupboardpermissions_set.filter(
-            group__in=user.groups.all(),
-            permission__name__in=permissions_name
-        ).count() == len(permissions_name):
-            return True
-        elif obj.board.userboardpermissions_set.filter(
-            user=user,
-            permission__name__in=permissions_name
-        ).count() == len(permissions_name):
-            return True
-        return False
+        return Boards.permissions.has_boards_access(
+            user,
+            obj.board,
+            permissions_name
+        )
