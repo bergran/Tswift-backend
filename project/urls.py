f6095037257d946801a14a49685d44ca63a84eac
Ángel <angel@kasfactory.net>
2018-03-31 01:52:21 +0200

    Configured settings to production

--- project/urls.py
+++ project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('api/v1/', include('v1.urls')),
     path('api/v1/', include('groups.urls')),
     path('api/v1/', include('rest_auth.urls')),
-    path('api/v1/registration/', include('rest_auth.registration.urls')),
+    path('api/v1/register/', include('rest_auth.registration.urls')),
     path('api/token/verify', verify_jwt_token),
     path('api/token/refresh', refresh_jwt_token)
 ]
