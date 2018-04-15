cf2980b799b76558dcbd350b943c0437ea2251f3
Ángel <angel@kasfactory.net>
2018-04-15 23:14:16 +0200

    Added filter owner

--- v1/filters/board_filter.py
+++ v1/filters/board_filter.py
@@ -6,6 +6,8 @@ from v1.models.Board import Boards
 
 
 class BoardFilters(django_filters.FilterSet):
+    owner = django_filters.CharFilter(name='owner__username')
+
     class Meta:
         model = Boards
-        fields = ('name', )
+        fields = ('name', 'owner')
