1c42e553c00699dbcb9f3968187b6fbd51de8246
Ángel <angel@kasfactory.net>
2018-04-16 20:49:22 +0200

    Updated board filters

--- v1/filters/board_filter.py
+++ v1/filters/board_filter.py
@@ -6,7 +6,8 @@ from v1.models.Board import Boards
 
 
 class BoardFilters(django_filters.FilterSet):
-    owner = django_filters.CharFilter(name='owner__username')
+    name = django_filters.CharFilter(lookup_expr='icontains')
+    owner = django_filters.CharFilter(name='owner__username', lookup_expr='icontains')
 
     class Meta:
         model = Boards
