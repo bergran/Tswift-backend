d9f8f36f22c8e8319471723a7c1d0271c283f1e6
Ángel Berhó <bergraan@gmail.com>
2018-05-13 16:49:11 +0200

    Added owner to board serializer

--- v1/serializers/boards/serializer.py
+++ v1/serializers/boards/serializer.py
@@ -6,6 +6,8 @@ from v1.models.Board import Boards
 
 
 class BoardSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(source='owner.username')
+
     class Meta:
         model = Boards
         fields = (
@@ -14,6 +16,7 @@ class BoardSerializer(serializers.ModelSerializer):
             'deleted',
             'date_created',
             'date_modified',
+            'owner',
             'states'
         )
 
@@ -22,7 +25,8 @@ class BoardSerializer(serializers.ModelSerializer):
             'deleted',
             'date_created',
             'date_modified',
-            'states'
+            'states',
+            'owner'
         )
 
     def create(self, validated_data):
